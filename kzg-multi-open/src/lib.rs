30b222b4e6ad3ddd809f55ffe92d3a2f1ce82e63
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-04-30 18:11:55 +0100

    embed trusted setup for now

--- kzg-multi-open/src/lib.rs
+++ kzg-multi-open/src/lib.rs
@@ -1,2 +1,5 @@
 pub mod commit_key;
 pub mod lincomb;
+// TODO: We can replace this with a file being embedded in the future.
+// This is simply the trusted setup file from the ethereum ceremony
+pub mod trusted_setup;
