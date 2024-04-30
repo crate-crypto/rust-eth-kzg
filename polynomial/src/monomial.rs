b3aa452d04dedeff753c9faceccb47550a72f62a
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-04-30 15:01:37 +0100

    add polynomial evaluation

--- polynomial/src/monomial.rs
+++ polynomial/src/monomial.rs
@@ -38,12 +38,43 @@ pub fn poly_sub(a: PolyCoeff, b: PolyCoeff) -> PolyCoeff {
     poly_add(a, neg_b)
 }
 
+/// Computes powers of a scalar up to a given degree.
+///
+/// Example: powers(x, 10) == [1, x, x^2, ..., x^10]
+fn powers_of(scalar: &Scalar, max_degree: usize) -> Vec<Scalar> {
+    let mut powers = Vec::with_capacity(max_degree);
+    powers.push(Scalar::from(1u64));
+    for i in 1..=max_degree {
+        powers.push(powers[i - 1] * scalar);
+    }
+    powers
+}
+
+/// Given a polynomial f(x) and a scalar z. This method will compute
+/// the result of f(z) and return the result.
+pub fn poly_eval(poly: &PolyCoeff, value: &Scalar) -> Scalar {
+    // If the scalar, we are evaluating at it zero, return the constant term
+    if value == &Scalar::from(0u64) {
+        return poly[0];
+    }
+
+    let powers = powers_of(value, poly.len());
+
+    let mut sum = Scalar::from(0u64);
+    for (power, coeff) in powers.iter().zip(poly.iter()) {
+        sum += coeff * power
+    }
+
+    sum
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
-    
+    use bls12_381::ff::Field;
+
     #[test]
-    pub fn basic_polynomial_add() {
+    fn basic_polynomial_add() {
         let a = vec![Scalar::from(1), Scalar::from(2), Scalar::from(3)];
         let b = vec![Scalar::from(4), Scalar::from(5), Scalar::from(6)];
         let c = vec![Scalar::from(5), Scalar::from(7), Scalar::from(9)];
@@ -56,14 +87,14 @@ mod tests {
     }
 
     #[test]
-    pub fn polynomial_neg() {
+    fn polynomial_neg() {
         let a = vec![Scalar::from(1), Scalar::from(2), Scalar::from(3)];
         let b = vec![-Scalar::from(1), -Scalar::from(2), -Scalar::from(3)];
         assert_eq!(poly_neg(a), b);
     }
 
     #[test]
-    pub fn basic_polynomial_subtraction() {
+    fn basic_polynomial_subtraction() {
         let a = vec![Scalar::from(1), Scalar::from(2), Scalar::from(3)];
         let b = vec![Scalar::from(4), Scalar::from(5), Scalar::from(6)];
         let c = vec![-Scalar::from(3), -Scalar::from(3), -Scalar::from(3)];
@@ -73,5 +104,33 @@ mod tests {
         let b = vec![Scalar::from(6), Scalar::from(7), Scalar::from(8)];
         let c = vec![-Scalar::from(2), -Scalar::from(2), -Scalar::from(8)];
         assert_eq!(poly_sub(a, b), c);
+    }
+
+    #[test]
+    fn powers_of_smoke_test() {
+        let scalar = Scalar::from(2u64);
+        let max_degree = 10;
+        let powers = powers_of(&scalar, max_degree);
+
+        assert_eq!(powers.len(), max_degree + 1);
+        
+        for i in 0..=max_degree {
+            assert_eq!(powers[i], scalar.pow_vartime(&[i as u64]));
+        }
+    }
+
+    #[test]
+    fn polynomial_evaluation() {
+        // f(x) = 1 + 2x + 3x^2
+        // f(2) = 1 + 2*2 + 3*2^2 = 1 + 4 + 12 = 17
+        let poly = vec![Scalar::from(1), Scalar::from(2), Scalar::from(3)];
+        let value = Scalar::from(2u64);
+        let result = Scalar::from(17u64);
+        assert!(poly_eval(&poly, &value) == result);
+
+        // f(0) = 1
+        let value = Scalar::from(0u64);
+        let result = Scalar::from(1u64);
+        assert!(poly_eval(&poly, &value) == result);
     }
 }
