321cc7c2b611381674c79add554836f798c1ac05
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-04-30 19:46:39 +0100

    cleaner separation between opening and commit key

--- kzg-multi-open/src/commit_key.rs
+++ kzg-multi-open/src/commit_key.rs
@@ -1,5 +1,5 @@
-use crate::lincomb::{g1_lincomb, g2_lincomb};
-use bls12_381::{G1Projective, G2Projective, Scalar};
+use crate::lincomb::g1_lincomb;
+use bls12_381::{G1Projective, Scalar};
 use polynomial::domain::Domain;
 
 // The key that is used to commit to polynomials in monomial form
@@ -10,7 +10,6 @@ use polynomial::domain::Domain;
 /// - `G` is some generator of the group
 pub struct CommitKey {
     g1s: Vec<G1Projective>,
-    g2s: Vec<G2Projective>,
 }
 
 // The key that is used to commit to polynomials in lagrange form
@@ -20,27 +19,18 @@ pub struct CommitKey {
 /// - `i` ranges from 0 to `degree`
 /// -  L_i is the i'th lagrange polynomial
 /// - `G` is some generator of the group
-///
-/// The G2 group elements are still in monomial form, see `CommitKey`
 pub struct CommitKeyLagrange {
     g1s: Vec<G1Projective>,
-    g2s: Vec<G2Projective>,
 }
 
 impl CommitKey {
-    pub fn new(g1_points: Vec<G1Projective>, g2_points: Vec<G2Projective>) -> CommitKey {
+    pub fn new(g1_points: Vec<G1Projective>) -> CommitKey {
         assert!(
             !g1_points.is_empty(),
             "cannot initialize `CommitKey` with no g1 points"
         );
-        assert!(
-            !g2_points.is_empty(),
-            "cannot initialize `CommitKey` with no g2 points"
-        );
-        CommitKey {
-            g1s: g1_points,
-            g2s: g2_points,
-        }
+
+        CommitKey { g1s: g1_points }
     }
 
     /// Convert the `CommitKey` to a `CommitKeyLagrange`
@@ -49,7 +39,6 @@ impl CommitKey {
     pub fn into_lagrange(self, domain: &Domain) -> CommitKeyLagrange {
         CommitKeyLagrange {
             g1s: domain.ifft_g1(self.g1s),
-            g2s: self.g2s,
         }
     }
 
@@ -60,27 +49,17 @@ impl CommitKey {
         // We use g1_lincomb because it is safer and the performance difference is negligible
         g1_lincomb(&self.g1s, &poly_coeff)
     }
-
-    /// Commit to `polynomial` in monomial form using the G2 group elements
-    pub fn commit_g2(&self, poly_coeff: &[Scalar]) -> G2Projective {
-        g2_lincomb(&self.g2s, &poly_coeff)
-    }
 }
 
 impl CommitKeyLagrange {
-    pub fn new(g1s: Vec<G1Projective>, g2s: Vec<G2Projective>) -> CommitKeyLagrange {
-        CommitKeyLagrange { g1s, g2s }
+    pub fn new(g1s: Vec<G1Projective>) -> CommitKeyLagrange {
+        CommitKeyLagrange { g1s }
     }
 
     /// Commit to a polynomial in lagrange form using the G1 group elements
     pub fn commit_g1(&self, polynomial: &[Scalar]) -> G1Projective {
         assert!(self.g1s.len() >= polynomial.len());
         g1_lincomb(&self.g1s[0..polynomial.len()], &polynomial)
-    }
-    /// Commit to a polynomial in lagrange form using the G2 group elements
-    pub fn commit_g2(&self, polynomial: &[Scalar]) -> G2Projective {
-        assert!(self.g2s.len() >= polynomial.len());
-        g2_lincomb(&self.g2s[0..polynomial.len()], &polynomial)
     }
 }
 
@@ -94,6 +73,6 @@ mod tests {
     fn eth_trusted_setup_deserializes() {
         // Just test that the trusted setup can be loaded
         let (g1s, g2s) = from_eth_setup();
-        let _ck = CommitKey::new(g1s, g2s);
+        let _ck = CommitKey::new(g1s);
     }
 }
