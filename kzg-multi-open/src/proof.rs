3cf3e75036ec36dabf12480023ef2e030804db1c
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-05-01 18:48:17 +0100

    - add a naive verification algorithm - Proof now corresponds to one multipoint opening proof and not all of them

--- kzg-multi-open/src/proof.rs
+++ kzg-multi-open/src/proof.rs
@@ -1,6 +1,7 @@
-use bls12_381::{G1Projective, Scalar};
+use bls12_381::{multi_pairings, G1Point, G1Projective, G2Point, G2Prepared, Scalar};
+use polynomial::monomial::{lagrange_interpolate, vanishing_poly};
 
-use crate::commit_key::CommitKey;
+use crate::{commit_key::CommitKey, opening_key::OpeningKey};
 
 /// A proof that a polynomial was opened at multiple points.
 ///
@@ -8,8 +9,8 @@ use crate::commit_key::CommitKey;
 /// using the techniques from [FK20](https://github.com/khovratovich/Kate/blob/master/Kate_amortized.pdf)
 /// since the points being opened are roots of unity.
 pub struct Proof {
-    /// Commitment to the `witness`'s or quotient polynomials
-    quotient_commitments: Vec<G1Projective>,
+    /// Commitment to the `witness` or quotient polynomial
+    quotient_commitment: G1Point,
     /// Evaluation of the polynomial at the input points.
     ///
     /// This implementation is only concerned with the case where the input points are roots of unity.
@@ -27,7 +28,48 @@ impl Proof {
     /// Verifies a multi-point opening proof.
     /// TODO: We may want to return a Result here so that errors can
     /// TODO be more descriptive.
-    pub fn verify(&self) -> bool {
-        todo!()
+    pub fn verify(
+        &self,
+        opening_key: &OpeningKey,
+        commitment: G1Point,
+        input_points: &[Scalar],
+    ) -> bool {
+        verify_multi_opening_naive(
+            opening_key,
+            commitment,
+            self.quotient_commitment,
+            input_points,
+            &self.output_points,
+        )
     }
+}
+
+/// Verifies a multi-opening proof using the general formula.
+///
+/// Note: This copies the exact implementation that the consensus-specs uses.
+pub fn verify_multi_opening_naive(
+    opening_key: &OpeningKey,
+    commitment: G1Point,
+    proof: G1Point,
+    input_points: &[Scalar],
+    output_points: &[Scalar],
+) -> bool {
+    // e([Commitment] - [r(x)], [1]) == e([Q(x)], [Z(X)])
+
+    let coordinates: Vec<_> = input_points
+        .iter()
+        .zip(output_points.iter())
+        .map(|(p, e)| (*p, *e))
+        .collect();
+    let r_x = lagrange_interpolate(&coordinates).unwrap();
+
+    let vanishing_poly = vanishing_poly(input_points);
+    let comm_vanishing_poly: G2Point = opening_key.commit_g2(&vanishing_poly).into();
+
+    let comm_r_x = opening_key.commit_g1(&r_x);
+    let comm_minus_r_x: G1Point = (G1Projective::from(commitment) - comm_r_x).into();
+    multi_pairings(&[
+        (&proof, &G2Prepared::from(comm_vanishing_poly)),
+        (&comm_minus_r_x, &G2Prepared::from(-opening_key.g2_gen())),
+    ])
 }
