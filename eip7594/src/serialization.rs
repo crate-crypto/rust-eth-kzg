eecf0308341b712a7505a867e486bd2e0a5f7532
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-05-03 15:02:44 +0100

    add method for serializing scalars and g1 points

--- eip7594/src/serialization.rs
+++ eip7594/src/serialization.rs
@@ -1,5 +1,5 @@
-use crate::constants::{BYTES_PER_FIELD_ELEMENT, BYTES_PER_G1_POINT};
-use bls12_381::{G1Projective, Scalar};
+use crate::constants::{BYTES_PER_FIELD_ELEMENT, BYTES_PER_G1_POINT, FIELD_ELEMENTS_PER_CELL};
+use bls12_381::{G1Point, G1Projective, Scalar};
 
 pub(crate) fn hex_to_bytes(hex_str: &str) -> Vec<u8> {
     hex::decode(hex_str).expect("malformed hex string")
@@ -31,4 +31,21 @@ pub(crate) fn deserialize_compressed_g1(point_bytes: &[u8]) -> G1Projective {
     let point_bytes: [u8; BYTES_PER_G1_POINT] =
         point_bytes.try_into().expect("point should be 48 bytes");
     G1Projective::from_compressed(&point_bytes).expect("cannot deserialize point")
+}
+pub(crate) fn serialize_g1_compressed(point: &G1Point) -> [u8; BYTES_PER_G1_POINT] {
+    point.to_compressed()
+}
+
+pub(crate) fn serialize_scalars_to_cell(scalars: &[Scalar]) -> Vec<u8> {
+    assert_eq!(
+        scalars.len(),
+        FIELD_ELEMENTS_PER_CELL,
+        "must have exactly {FIELD_ELEMENTS_PER_CELL} scalars to serialize to a cell"
+    );
+
+    let mut bytes = Vec::with_capacity(FIELD_ELEMENTS_PER_CELL * BYTES_PER_FIELD_ELEMENT);
+    for scalar in scalars {
+        bytes.extend_from_slice(&scalar.to_bytes_be());
+    }
+    bytes
 }
