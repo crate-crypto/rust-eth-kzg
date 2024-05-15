e44bba0cba9b50fbfb68554d682e963b3f7777a6
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-05-15 23:15:51 +0100

    put c header in c/build/

--- bindings/c/build.rs
+++ bindings/c/build.rs
@@ -2,13 +2,16 @@ use std::env;
 use std::path::PathBuf;
 
 /// The directory where the generated header file will be written.
-const DIR_FOR_HEADER: &str = "lib";
+const DIR_FOR_HEADER: &str = "build";
 
 fn main() {
     println!("cargo:rerun-if-changed=src/");
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let package_name = env::var("CARGO_PKG_NAME").unwrap();
-    let output_file = PathBuf::from(&crate_dir)
+
+    let path_to_crate_dir = PathBuf::from(&crate_dir);
+
+    let output_file = PathBuf::from(&path_to_crate_dir)
         .join(DIR_FOR_HEADER)
         .join(format!("{}.h", package_name))
         .display()
