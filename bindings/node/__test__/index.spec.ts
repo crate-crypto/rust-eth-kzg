8fe68a99dff17747a7589d667a2abb821d8f7171
matthewkeil <me@matthewkeil.com>
2024-05-16 18:00:58 +0300

    test: get tests working

--- bindings/node/__test__/index.spec.ts
+++ bindings/node/__test__/index.spec.ts
@@ -3,6 +3,48 @@ import {ProverContextJs, VerifierContextJs} from "../index.js";
 const proverContext = new ProverContextJs();
 const verifierContext = new VerifierContextJs();
 
-describe("sum from native", (t) => {
-  t.is(sum(1, 2), 3);
+describe("ProverContext", () => {
+  describe("blobToKzgCommitment", () => {
+    test("function exists", () => {
+      expect(proverContext.blobToKzgCommitment).toBeDefined();
+    });
+  });
+  describe("asyncBlobToKzgCommitment", () => {
+    test("function exists", () => {
+      expect(proverContext.asyncBlobToKzgCommitment).toBeDefined();
+    });
+  });
+  describe("computeCellsAndKzgProofs", () => {
+    test("function exists", () => {
+      expect(proverContext.computeCellsAndKzgProofs).toBeDefined();
+    });
+  });
+  describe("asyncComputeCellsAndKzgProofs", () => {
+    test("function exists", () => {
+      expect(proverContext.asyncComputeCellsAndKzgProofs).toBeDefined();
+    });
+  });
+  describe("computeCells", () => {
+    test("function exists", () => {
+      expect(proverContext.computeCells).toBeDefined();
+    });
+  });
+  describe("asyncComputeCells", () => {
+    test("function exists", () => {
+      expect(proverContext.asyncComputeCells).toBeDefined();
+    });
+  });
+});
+
+describe("VerifierContext", () => {
+  describe("verifyCellKzgProof", () => {
+    test("function exists", () => {
+      expect(verifierContext.verifyCellKzgProof).toBeDefined();
+    });
+  });
+  describe("asyncVerifyCellKzgProof", () => {
+    test("function exists", () => {
+      expect(verifierContext.asyncVerifyCellKzgProof).toBeDefined();
+    });
+  });
 });
