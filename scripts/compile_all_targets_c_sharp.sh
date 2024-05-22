517e723a713024dded74225f060b7f03d4a7135e
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-05-22 13:27:29 +0100

    chore: remove now redundant ConsoleApp1 directory

--- scripts/compile_all_targets_c_sharp.sh
+++ scripts/compile_all_targets_c_sharp.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-
 chmod a+x scripts/compile_all_targets_c.sh
 
 # First compile the c code to make sure we have the libraries
 sh scripts/compile_all_targets_c.sh
 
 # Copy the static lib + headers into the c sharp bindings folder
-cp -R ./bindings/c/build ./bindings/csharp/ConsoleApp1/runtimes
+cp -R ./bindings/c/build ./bindings/csharp/runtimes
