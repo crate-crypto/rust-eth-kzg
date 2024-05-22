8c5ebbb2062937469d6dac6545a90a77c7bf7020
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-05-22 20:29:13 +0100

    chore: add scripts to compile and copy static/dynamic libraries

--- scripts/compile_c.sh
+++ scripts/compile_c.sh
@@ -1,31 +1,65 @@
 #!/bin/bash
 
-# First build in release mode, this should build the c crate
-cargo build --release
 
-# Determine the operating system
-OS="$(uname)"
-ARCH="$(uname -m)"
-echo "Detected OS: $OS"
-echo "Detected architecture: $ARCH"
+# Function to display usage information
+usage() {
+    echo "Usage: $0 [OS] [ARCH] [LIB_NAME] [LIB_TYPE] [OUT_DIR]"
+    echo "Compile the project for the specified OS, architecture, library name, library type, and output directory."
+    echo "If no OS and ARCH are provided, it defaults to the current system's OS and architecture."
+    echo "If no LIB_NAME is provided, it defaults to 'c_peerdas_kzg'."
+    echo "If no LIB_TYPE is provided, it defaults to 'both'."
+    echo "If no OUT_DIR is provided, it defaults to './bindings/c/build'."
+    echo
+    echo "Arguments:"
+    echo "  OS        Operating system (e.g., Linux, Darwin, MINGW64_NT)"
+    echo "  ARCH      Architecture (e.g., x86_64, arm64)"
+    echo "  LIB_NAME  Library name (e.g., c_peerdas_kzg)"
+    echo "  LIB_TYPE  Library type to copy (static, dynamic, or both)"
+    echo "  OUT_DIR   Output directory for the compiled libraries"
+    echo
+    echo "Examples:"
+    echo "  $0                                        # Uses the system's OS and architecture, copies both libraries to the default directory with the default library name"
+    echo "  $0 Linux x86_64 my_lib static             # Compiles for Linux on x86_64 and copies only static libraries to the default directory with the library name 'my_lib'"
+    echo "  $0 Darwin arm64 my_lib dynamic ./out/dir  # Compiles for macOS on ARM (Apple Silicon) and copies only dynamic libraries to './out/dir' with the library name 'my_lib'"
+    exit 1
+}
+
+
+# Check for help flag
+if [[ "$1" == "-h" || "$1" == "--help" ]]; then
+    usage
+fi
+
+# Determine the operating system, architecture, library name, library type, and output directory if not provided
+OS="${1:-$(uname)}"
+ARCH="${2:-$(uname -m)}"
+LIB_NAME="${3:-c_peerdas_kzg}"
+LIB_TYPE="${4:-both}"
+OUT_DIR="${5:-./bindings/c/build}"
+echo "Detected/Provided OS: $OS"
+echo "Detected/Provided architecture: $ARCH"
+echo "Library name: $LIB_NAME"
+echo "Library type to copy: $LIB_TYPE"
+echo "Output directory: $OUT_DIR"
+
+STATIC_LIB_NAME=""
+DYNAMIC_LIB_NAME=""
+TARGET_NAME=""
 
 case "$OS" in
     "Darwin")
-        echo "Running on macOS"
         case "$ARCH" in
             "arm64")
-                echo "Detected macOS on ARM (Apple Silicon)"
                 # Copy static and shared libraries for macOS ARM
-                mkdir -p ./bindings/c/build/darwin-aarch64
-                cp -R target/release/libc_peerdas_kzg.dylib ./bindings/c/build/darwin-aarch64/
-                cp -R target/release/libc_peerdas_kzg.a ./bindings/c/build/darwin-aarch64/
+                TARGET_NAME="aarch64-apple-darwin"
+                STATIC_LIB_NAME="lib${LIB_NAME}.a"
+                DYNAMIC_LIB_NAME="lib${LIB_NAME}.dylib"
                 ;;
             "x86_64")
-                echo "Detected macOS on Intel"
                 # Copy static and shared libraries for macOS Intel
-                mkdir -p ./bindings/c/build/darwin-amd64
-                cp -R target/release/libc_peerdas_kzg.dylib ./bindings/c/build/darwin-amd64/
-                cp -R target/release/libc_peerdas_kzg.a ./bindings/c/build/darwin-amd64/
+                TARGET_NAME="x86_64-apple-darwin"
+                STATIC_LIB_NAME="lib${LIB_NAME}.a"
+                DYNAMIC_LIB_NAME="lib${LIB_NAME}.dylib"
                 ;;
             *)
                 echo "Unsupported macOS architecture: $ARCH"
@@ -34,21 +68,18 @@ case "$OS" in
         esac
         ;;
     "Linux")
-        echo "Running on Linux"
         case "$ARCH" in
             "arm64")
-                echo "Detected Linux on ARM"
                 # Copy static and shared libraries for Linux ARM
-                mkdir -p ./bindings/c/build/linux-aarch64
-                cp target/release/libc_peerdas_kzg.a ./bindings/c/build/linux-aarch64/
-                cp target/release/libc_peerdas_kzg.so ./bindings/c/build/linux-aarch64/
+                TARGET_NAME="aarch64-unknown-linux-gnu"
+                STATIC_LIB_NAME="lib${LIB_NAME}.a"
+                DYNAMIC_LIB_NAME="lib${LIB_NAME}.so"
                 ;;
             "x86_64")
-                echo "Detected Linux on Intel"
                 # Copy static and shared libraries for Linux Intel
-                mkdir -p ./bindings/c/build/linux-amd64
-                cp target/release/libc_peerdas_kzg.a ./bindings/c/build/linux-amd64/
-                cp target/release/libc_peerdas_kzg.so ./bindings/c/build/linux-amd64/
+                TARGET_NAME="x86_64-unknown-linux-gnu"
+                STATIC_LIB_NAME="lib${LIB_NAME}.a"
+                DYNAMIC_LIB_NAME="lib${LIB_NAME}.so"
                 ;;
             *)
                 echo "Unsupported Linux architecture: $ARCH"
@@ -57,14 +88,37 @@ case "$OS" in
         esac
         ;;
     "MINGW64_NT"|"CYGWIN_NT")
-        echo "Running on Windows"
-        mkdir -p ./bindings/c/build/windows/
-        # Copy static and shared libraries for Windows
-        cp target/release/libc_peerdas_kzg.lib ./bindings/c/build/windows/
-        cp target/release/libc_peerdas_kzg.dll ./bindings/c/build/windows/
+        TARGET_NAME="x86_64-pc-windows-gnu"
+        STATIC_LIB_NAME="lib${LIB_NAME}.a"
+        DYNAMIC_LIB_NAME="${LIB_NAME}.dll"
         ;;
     *)
         echo "Unsupported OS: $OS"
         exit 1
         ;;
 esac
+
+echo "Compiling for target: $TARGET_NAME"
+./scripts/check_if_rustup_target_installed.sh $TARGET_NAME
+
+# Check the exit code 
+if [ $? -eq 0 ]; then
+  echo "The default Rust target is installed."
+else
+  echo "The default Rust target is not installed."
+  exit 1
+fi
+
+cargo build --release --target=$TARGET_NAME
+
+# Create the output directory if it doesn't exist
+mkdir -p "$OUT_DIR/$TARGET_NAME"
+
+# Copy the libraries to the specified output directory
+if [ "$LIB_TYPE" == "static" ] || [ "$LIB_TYPE" == "both" ]; then
+    cp -R target/$TARGET_NAME/release/$STATIC_LIB_NAME "$OUT_DIR/$TARGET_NAME/"
+fi
+
+if [ "$LIB_TYPE" == "dynamic" ] || [ "$LIB_TYPE" == "both" ]; then
+    cp -R target/$TARGET_NAME/release/$DYNAMIC_LIB_NAME "$OUT_DIR/$TARGET_NAME/"
+fi
