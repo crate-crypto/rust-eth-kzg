83ee1010071961ca3f7794080d584bc5783772c8
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-05-22 21:06:35 +0100

    chore: use variable names to make some parameters clearer

--- scripts/compile_all_targets_c.sh
+++ scripts/compile_all_targets_c.sh
@@ -3,8 +3,12 @@
 
 # Compile dynamic libraries for all relevant targets
 # and place them in the bindings/c/build directory
-./scripts/compile_to_native.sh Darwin arm64 c_peerdas_kzg dynamic ./bindings/c/build
-./scripts/compile_to_native.sh Darwin x86_64 c_peerdas_kzg dynamic ./bindings/c/build
-./scripts/compile_to_native.sh Windows x86_64 c_peerdas_kzg dynamic ./bindings/c/build
-./scripts/compile_to_native.sh Linux x86_64 c_peerdas_kzg dynamic ./bindings/c/build
-./scripts/compile_to_native.sh Linux arm64 c_peerdas_kzg dynamic ./bindings/c/build
+
+OUT_DIR="./bindings/c/build"
+LIB_TYPE="dynamic"
+LIB_NAME="c_peerdas_kzg"
+./scripts/compile_to_native.sh Darwin arm64 $LIB_NAME $LIB_TYPE $OUT_DIR
+./scripts/compile_to_native.sh Darwin x86_64 $LIB_NAME $LIB_TYPE $OUT_DIR
+./scripts/compile_to_native.sh Windows x86_64 $LIB_NAME $LIB_TYPE $OUT_DIR
+./scripts/compile_to_native.sh Linux x86_64 $LIB_NAME $LIB_TYPE $OUT_DIR
+./scripts/compile_to_native.sh Linux arm64 $LIB_NAME $LIB_TYPE $OUT_DIR
