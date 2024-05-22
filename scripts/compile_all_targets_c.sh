b32ff0662dfd979159a1838b4ff18bebdf277957
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-05-22 21:04:02 +0100

    chore: rename `compile_c` to `compile_to_native`

--- scripts/compile_all_targets_c.sh
+++ scripts/compile_all_targets_c.sh
@@ -3,8 +3,8 @@
 
 # Compile dynamic libraries for all relevant targets
 # and place them in the bindings/c/build directory
-./scripts/compile_c.sh Darwin arm64 c_peerdas_kzg dynamic ./bindings/c/build
-./scripts/compile_c.sh Darwin x86_64 c_peerdas_kzg dynamic ./bindings/c/build
-./scripts/compile_c.sh Windows x86_64 c_peerdas_kzg dynamic ./bindings/c/build
-./scripts/compile_c.sh Linux x86_64 c_peerdas_kzg dynamic ./bindings/c/build
-./scripts/compile_c.sh Linux arm64 c_peerdas_kzg dynamic ./bindings/c/build
+./scripts/compile_to_native.sh Darwin arm64 c_peerdas_kzg dynamic ./bindings/c/build
+./scripts/compile_to_native.sh Darwin x86_64 c_peerdas_kzg dynamic ./bindings/c/build
+./scripts/compile_to_native.sh Windows x86_64 c_peerdas_kzg dynamic ./bindings/c/build
+./scripts/compile_to_native.sh Linux x86_64 c_peerdas_kzg dynamic ./bindings/c/build
+./scripts/compile_to_native.sh Linux arm64 c_peerdas_kzg dynamic ./bindings/c/build
