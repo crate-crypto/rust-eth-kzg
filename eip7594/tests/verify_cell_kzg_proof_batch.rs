5ddda30e1eb5289fbeafca6fa002cd862603b962
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-05-29 15:40:53 +0100

    chore:  add batch verify tests

---
+++ eip7594/tests/verify_cell_kzg_proof_batch.rs
@@ -0,0 +1,114 @@
+use common::collect_test_files;
+use eip7594::verifier::VerifierError;
+use serde_::TestVector;
+use std::fs;
+
+mod common;
+
+mod serde_ {
+    use crate::common::{bytes_from_hex, UnsafeBytes};
+
+    use super::common::cell_from_hex;
+    use serde::Deserialize;
+
+    #[derive(Deserialize)]
+    struct YamlInput {
+        row_commitments: Vec<String>,
+        row_indices: Vec<u64>,
+        column_indices: Vec<u64>,
+        cells: Vec<String>,
+        proofs: Vec<String>,
+    }
+
+    type YamlOutput = bool;
+
+    #[derive(Deserialize)]
+    struct YamlTestVector {
+        input: YamlInput,
+        output: Option<YamlOutput>,
+    }
+
+    pub struct TestVector {
+        pub row_commitments: Vec<UnsafeBytes>,
+        pub row_indices: Vec<u64>,
+        pub column_indices: Vec<u64>,
+        pub cells: Vec<UnsafeBytes>,
+        pub proofs: Vec<UnsafeBytes>,
+        pub output: Option<bool>,
+    }
+
+    impl TestVector {
+        pub fn from_str(yaml_data: &str) -> Self {
+            let yaml_test_vector: YamlTestVector = serde_yaml::from_str(yaml_data).unwrap();
+            TestVector::from(yaml_test_vector)
+        }
+    }
+
+    impl From<YamlTestVector> for TestVector {
+        fn from(yaml_test_vector: YamlTestVector) -> Self {
+            let row_commitments = yaml_test_vector
+                .input
+                .row_commitments
+                .into_iter()
+                .map(|commitment| bytes_from_hex(&commitment))
+                .collect();
+            let cells = yaml_test_vector
+                .input
+                .cells
+                .into_iter()
+                .map(|cell| cell_from_hex(&cell))
+                .collect();
+            let proofs: Vec<_> = yaml_test_vector
+                .input
+                .proofs
+                .into_iter()
+                .map(|proof| bytes_from_hex(&proof))
+                .collect();
+            let row_indices = yaml_test_vector.input.row_indices;
+            let column_indices = yaml_test_vector.input.column_indices;
+
+            let output = yaml_test_vector.output;
+
+            TestVector {
+                row_commitments,
+                row_indices,
+                column_indices,
+                cells,
+                proofs,
+                output,
+            }
+        }
+    }
+}
+
+const TEST_DIR: &str = "../consensus_test_vectors/verify_cell_kzg_proof_batch";
+#[test]
+fn test_verify_cell_kzg_proof_batch() {
+    let test_files = collect_test_files(TEST_DIR).unwrap();
+
+    let verifier_context = eip7594::verifier::VerifierContext::new();
+
+    for test_file in test_files {
+        let yaml_data = fs::read_to_string(&test_file).unwrap();
+        let test = TestVector::from_str(&yaml_data);
+
+        match verifier_context.verify_cell_kzg_proof_batch(
+            test.row_commitments.iter().map(|v| v.as_slice()).collect(),
+            test.row_indices,
+            test.column_indices,
+            test.cells.iter().map(|v| v.as_slice()).collect(),
+            test.proofs.iter().map(|v| v.as_slice()).collect(),
+        ) {
+            Ok(_) => {
+                // We arrive at this point if the proof verified as true
+                assert!(test.output.unwrap())
+            }
+            Err(VerifierError::InvalidProof) => {
+                assert!(test.output.unwrap() == false);
+            }
+            Err(_) => {
+                assert!(test.output.is_none());
+            }
+        };
+    }
+}
