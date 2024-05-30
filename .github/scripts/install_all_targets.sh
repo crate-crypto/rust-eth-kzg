a092feedb188caa14f95a662d09c8b71941c9808
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-05-30 18:57:38 +0100

    chore: install linkers

--- .github/scripts/install_all_targets.sh
+++ .github/scripts/install_all_targets.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# This assumes we are on mac, for the CI we will be running this in Mac
+brew install SergioBenitez/osxct/x86_64-unknown-linux-gnu
+brew tap messense/homebrew-macos-cross-toolchains
+brew install aarch64-unknown-linux-gnu
+
 rustup target add x86_64-unknown-linux-gnu
 rustup target add aarch64-unknown-linux-gnu
 rustup target add x86_64-pc-windows-gnu
