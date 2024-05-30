cd50d5a6d8e5484b29acf0657b30f4b30e341387
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-05-30 17:28:35 +0100

    chore: add install_all_targets script

---
+++ .github/scripts/install_all_targets.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rustup target add x86_64-unknown-linux-gnu
+rustup target add aarch64-unknown-linux-gnu
+rustup target add x86_64-pc-windows-gnu
+rustup target add x86_64-apple-darwin
+rustup target add aarch64-apple-darwin
