e8630a8c3f8665d82838971a4db2517d51a0de44
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-05-31 21:12:30 +0100

    chore: add project for tests

---
+++ bindings/csharp/PeerDASKZG.test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace PeerDASKZG.test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.AreEqual(true, true);
+    }
+}
