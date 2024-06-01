62023ccea324c52f9e7ae9b2bca705668a45a478
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-06-01 20:48:58 +0100

    chore: add BlobToKzgCommitment to wrapper methods in class

--- bindings/csharp/PeerDASKZG.bindings/peerdaskzg.cs
+++ bindings/csharp/PeerDASKZG.bindings/peerdaskzg.cs
@@ -8,4 +8,38 @@ public static partial class PeerDASKZG
         return InternalProverContextNew();
     }
 
+    public static void ProverContextFree(IntPtr proverContextPtr)
+    {
+        InternalProverContextFree(proverContextPtr);
+    }
+
+    public static unsafe byte[] BlobToKzgCommitment(byte[] blob, IntPtr proverContextPtr)
+    {
+        // Sanity check input
+        ThrowOnInvalidLength(blob, nameof(blob), 4096 * 32);
+
+        byte[] commitment = new byte[48];
+        Result result = InternalBlobToKzgCommitment(proverContextPtr, blob, commitment);
+        ThrowOnError(result);
+        return commitment;
+    }
+
+    // Below error handling was copied from ckzg
+    private static void ThrowOnError(Result result)
+    {
+        switch (result)
+        {
+            case Result.Err: throw new ArgumentException("an error occurred from the bindings");
+            case Result.Ok:
+                return;
+            default:
+                throw new ApplicationException("PeerDASKZG returned an unexpected result variant");
+        }
+    }
+
+    private static void ThrowOnInvalidLength(ReadOnlySpan<byte> data, string fieldName, int expectedLength)
+    {
+        if (data.Length != expectedLength)
+            throw new ArgumentException("Invalid data size", fieldName);
+    }
 }
