0e386510fedca56e1abbaf3901e93c5cc2880622
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-06-05 15:25:52 +0100

    chore: update nim bindings test

--- bindings/nim/nim_code/tests/test_smoke.nim
+++ bindings/nim/nim_code/tests/test_smoke.nim
@@ -2,12 +2,7 @@ import unittest
 
 import nim_peerdas_kzg
 
-test "prover context new and free":
-  let context = prover_context_new()
+test "context new and free":
+  let context = peerdas_context_new()
   check context != nil
-  prover_context_free(context)
-
-test "verifier context new and free":
-  let context = verifier_context_new()
-  check context != nil
-  verifier_context_free(context)
+  peerdas_context_free(context)
