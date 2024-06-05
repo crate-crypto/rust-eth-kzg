0c114bbf313cc90158f006238d5ac2d956d1a8d1
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-06-05 16:37:25 +0100

    chore: use C crate internally for java bindings

--- bindings/java/rust_code/src/lib.rs
+++ bindings/java/rust_code/src/lib.rs
@@ -1,6 +1,9 @@
 use c_peerdas_kzg::PeerDASContext;
+use c_peerdas_kzg::BYTES_PER_COMMITMENT;
 use jni::objects::JByteArray;
 use jni::objects::JClass;
+use jni::objects::JObject;
+use jni::objects::JValue;
 use jni::sys::{jboolean, jlong};
 use jni::JNIEnv;
 
@@ -9,9 +12,7 @@ pub unsafe extern "system" fn Java_ethereum_cryptography_LibPeerDASKZG_peerDASCo
     _env: JNIEnv,
     _class: JClass,
 ) -> jlong {
-    let context = PeerDASContext::new();
-
-    Box::into_raw(Box::new(context)) as jlong
+    c_peerdas_kzg::peerdas_context_new() as jlong
 }
 
 #[no_mangle]
@@ -20,7 +21,7 @@ pub unsafe extern "system" fn Java_ethereum_cryptography_LibPeerDASKZG_peerDASCo
     _class: JClass,
     ctx_ptr: jlong,
 ) {
-    let _boxed_prover_context = Box::from_raw(ctx_ptr as *mut PeerDASContext);
+    c_peerdas_kzg::peerdas_context_free(ctx_ptr as *mut PeerDASContext);
 }
 
 #[no_mangle]
@@ -30,47 +31,69 @@ pub unsafe extern "system" fn Java_ethereum_cryptography_LibPeerDASKZG_computeCe
     ctx_ptr: jlong,
     blob: JByteArray<'local>,
 ) -> JByteArray<'local> {
-    let ctx = &mut *(ctx_ptr as *mut PeerDASContext);
+    let ctx = ctx_ptr as *const PeerDASContext;
     let blob = env.convert_byte_array(blob).unwrap();
-    let cells = ctx.prover_ctx().unwrap().compute_cells(&blob).unwrap();
 
-    let flattened_cells = cells
-        .iter()
-        .flat_map(|cell| cell.iter())
-        .copied()
-        .collect::<Vec<u8>>();
+    let mut out_cells = vec![0u8; c_peerdas_kzg::NUM_BYTES_CELLS as usize];
 
-    return env.byte_array_from_slice(&flattened_cells).unwrap();
+    // TODO: handle CResult and throw
+    c_peerdas_kzg::compute_cells(
+        ctx,
+        blob.len() as u64,
+        blob.as_ptr(),
+        out_cells.as_mut_ptr(),
+    );
+
+    return env.byte_array_from_slice(&out_cells).unwrap();
+}
+
+#[repr(C)]
+pub struct CellsAndProofs {
+    pub cells: [u8; c_peerdas_kzg::NUM_BYTES_CELLS as usize],
+    pub proofs: [u8; c_peerdas_kzg::NUM_BYTES_PROOFS as usize],
 }
 
 #[no_mangle]
 pub unsafe extern "system" fn Java_ethereum_cryptography_LibPeerDASKZG_computeCellsAndKZGProofs<
     'local,
 >(
-    env: JNIEnv<'local>,
+    mut env: JNIEnv<'local>,
     _class: JClass,
     ctx_ptr: jlong,
     blob: JByteArray<'local>,
-) -> JByteArray<'local> {
-    let ctx = &mut *(ctx_ptr as *mut PeerDASContext);
-
+) -> JObject<'local> {
+    let ctx = ctx_ptr as *const PeerDASContext;
     let blob = env.convert_byte_array(blob).unwrap();
 
-    let (cells, proofs) = ctx
-        .prover_ctx()
-        .unwrap()
-        .compute_cells_and_kzg_proofs(&blob)
-        .unwrap();
+    let mut cells_and_proofs = CellsAndProofs {
+        cells: [0u8; c_peerdas_kzg::NUM_BYTES_CELLS as usize],
+        proofs: [0u8; c_peerdas_kzg::NUM_BYTES_PROOFS as usize],
+    };
 
-    let flattened_proofs_and_cells: Vec<u8> = cells
-        .into_iter()
-        .zip(proofs.into_iter())
-        .flat_map(|(cell, proof)| cell.into_iter().chain(proof.into_iter()))
-        .collect();
+    // TODO: handle CResult and throw
+    c_peerdas_kzg::compute_cells_and_kzg_proofs(
+        ctx,
+        blob.len() as u64,
+        blob.as_ptr(),
+        cells_and_proofs.cells.as_mut_ptr(),
+        cells_and_proofs.proofs.as_mut_ptr(),
+    );
 
-    return env
-        .byte_array_from_slice(&flattened_proofs_and_cells)
+    // Create a new instance of the CellsAndProofs class in Java
+    let cells_and_proofs_class = env
+        .find_class("ethereum/cryptography/CellsAndProofs")
         .unwrap();
+    let cells_and_proofs_obj = env
+        .new_object(
+            cells_and_proofs_class,
+            "([B[B)V",
+            &[
+                JValue::Object(&env.byte_array_from_slice(&cells_and_proofs.cells).unwrap()),
+                JValue::Object(&env.byte_array_from_slice(&cells_and_proofs.proofs).unwrap()),
+            ],
+        )
+        .unwrap();
+    cells_and_proofs_obj
 }
 
 #[no_mangle]
@@ -82,15 +105,15 @@ pub unsafe extern "system" fn Java_ethereum_cryptography_LibPeerDASKZG_blobToKZG
     ctx_ptr: jlong,
     blob: JByteArray<'local>,
 ) -> JByteArray<'local> {
-    let ctx = &mut *(ctx_ptr as *mut PeerDASContext);
+    let ctx = ctx_ptr as *const PeerDASContext;
     let blob = env.convert_byte_array(blob).unwrap();
-    let commitment = ctx
-        .prover_ctx()
-        .unwrap()
-        .blob_to_kzg_commitment(&blob)
-        .unwrap();
 
-    return env.byte_array_from_slice(&commitment).unwrap();
+    let mut out = vec![0u8; BYTES_PER_COMMITMENT as usize];
+
+    // TODO: handle CResult and throw
+    c_peerdas_kzg::blob_to_kzg_commitment(ctx, blob.len() as u64, blob.as_ptr(), out.as_mut_ptr());
+
+    return env.byte_array_from_slice(&out).unwrap();
 }
 
 #[no_mangle]
@@ -105,17 +128,28 @@ pub unsafe extern "system" fn Java_ethereum_cryptography_LibPeerDASKZG_verifyCel
     cell: JByteArray<'local>,
     proof_bytes: JByteArray<'local>,
 ) -> jboolean {
-    let ctx = &mut *(ctx_ptr as *mut PeerDASContext);
+    let ctx = ctx_ptr as *const PeerDASContext;
 
     let commitment_bytes = env.convert_byte_array(&commitment_bytes).unwrap();
     let cell_id = cell_id as u64;
     let cell = env.convert_byte_array(&cell).unwrap();
     let proof_bytes = env.convert_byte_array(&proof_bytes).unwrap();
 
-    return jboolean::from(
-        ctx.verifier_ctx()
-            .unwrap()
-            .verify_cell_kzg_proof(&commitment_bytes, cell_id, &cell, &proof_bytes)
-            .is_ok(),
+    let mut verified = false;
+    let verified_ptr: *mut bool = (&mut verified) as *mut bool;
+
+    // TODO: handle CResult and throw
+    c_peerdas_kzg::verify_cell_kzg_proof(
+        ctx,
+        cell.len() as u64,
+        cell.as_ptr(),
+        commitment_bytes.len() as u64,
+        commitment_bytes.as_ptr(),
+        cell_id,
+        proof_bytes.len() as u64,
+        proof_bytes.as_ptr(),
+        verified_ptr,
     );
+
+    return jboolean::from(verified);
 }
