3bfbffc9ed6acd4d9a56783538dd2ea8b5a71d0d
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-06-05 12:32:53 +0100

    chore: get the package name of the c crate in the workspace

--- bindings/csharp/rust_code/build.rs
+++ bindings/csharp/rust_code/build.rs
@@ -1,12 +1,40 @@
+use std::{env, fs, path::PathBuf};
+
+use toml::Value;
+
 fn main() {
+    let package_name_of_c_crate = get_package_name_of_c_crate();
+
     csbindgen::Builder::default()
-        .input_extern_file("../../../c/src/lib.rs")
-        // .input_extern_file("src/lib.rs")
+        .input_extern_file("../../c/src/lib.rs")
         .csharp_namespace("PeerDasKZG")
-        .csharp_dll_name("c_peerdas_kzg") // TODO: pull in the package name from c crate
+        .csharp_dll_name(package_name_of_c_crate)
         .csharp_class_name("PeerDasKZG")
         .csharp_use_nint_types(false)
         .generate_csharp_file("../dotnet/NativeMethods.g.cs")
         .unwrap();
-    //
+}
+
+fn get_package_name_of_c_crate() -> String {
+    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let crate_dir = PathBuf::from(crate_dir);
+
+    // Go up two directories to be at bindings parent directory
+    let parent = crate_dir.parent().unwrap().parent().unwrap().to_path_buf();
+    let path_to_c_crate = parent.join("c");
+    let path_to_c_crate_cargo_toml = path_to_c_crate.join("Cargo.toml");
+
+    // Read the Cargo.toml of the other crate
+    let cargo_toml =
+        fs::read_to_string(path_to_c_crate_cargo_toml).expect("Failed to read Cargo.toml");
+
+    // Parse the Cargo.toml content
+    let cargo_toml: Value = cargo_toml.parse().expect("Failed to parse Cargo.toml");
+
+    // Access the package name from the parsed Cargo.toml
+    let package_name = cargo_toml["package"]["name"]
+        .as_str()
+        .expect("Failed to get package name");
+
+    package_name.to_string()
 }
