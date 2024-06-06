2d01b4a01989e164468b955b1a95df536096dc39
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-06-07 00:22:54 +0100

    chore: remove smoke test method

--- bindings/java/java_code/src/test/java/ethereum/cryptography/LibPeerDASKZGTest.java
+++ bindings/java/java_code/src/test/java/ethereum/cryptography/LibPeerDASKZGTest.java
@@ -27,17 +27,6 @@ public class LibPeerDASKZGTest {
         ctx_ptr = LibPeerDASKZG.peerDASContextNew();
     }
 
-    @Test
-    public void testCanCallLibrary() {
-        final int blobSizeInBytes = 4096 * 32;
-        byte[] byteArray = new byte[blobSizeInBytes];
-
-        java.util.Arrays.fill(byteArray, (byte) 0);
-
-        byte[] res = LibPeerDASKZG.computeCells(ctx_ptr, byteArray);
-        CellsAndProofs res2 = LibPeerDASKZG.computeCellsAndKZGProofs(ctx_ptr, byteArray);
-        byte[] res3 = LibPeerDASKZG.blobToKZGCommitment(ctx_ptr, byteArray);
-    }
 
     @ParameterizedTest
     @MethodSource("ethereum.cryptography.TestUtils#getBlobToKzgCommitmentTests")
