68d38a528ee23bb96ad2a2f1290bf190b7f0e443
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-06-08 20:28:58 +0100

    chore: clarify comment on windows extension

--- bindings/nim/nim_code/src/nim_peerdas_kzg/bindings.nim
+++ bindings/nim/nim_code/src/nim_peerdas_kzg/bindings.nim
@@ -4,7 +4,7 @@ import ../header
 export header
 
 when defined(windows):
-  # For gnu toolchain, the name is .a
+  # For gnu toolchain, the extension is .a since it uses the linux toolchain
   # This will need to be changed if we switch to the msvc toolchain
   const libName = "libc_peerdas_kzg.a"
 else:
