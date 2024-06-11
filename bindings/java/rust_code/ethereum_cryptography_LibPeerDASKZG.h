5a5583821bcaaa5dc393b1d45e237e18fc5f0672
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-06-11 21:41:30 +0100

    chore: update generated java bindings header file

--- bindings/java/rust_code/ethereum_cryptography_LibPeerDASKZG.h
+++ bindings/java/rust_code/ethereum_cryptography_LibPeerDASKZG.h
@@ -7,30 +7,18 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-#undef ethereum_cryptography_LibPeerDASKZG_BYTES_PER_G1
-#define ethereum_cryptography_LibPeerDASKZG_BYTES_PER_G1 48L
-#undef ethereum_cryptography_LibPeerDASKZG_BYTES_PER_G2
-#define ethereum_cryptography_LibPeerDASKZG_BYTES_PER_G2 96L
 #undef ethereum_cryptography_LibPeerDASKZG_BYTES_PER_COMMITMENT
 #define ethereum_cryptography_LibPeerDASKZG_BYTES_PER_COMMITMENT 48L
 #undef ethereum_cryptography_LibPeerDASKZG_BYTES_PER_PROOF
 #define ethereum_cryptography_LibPeerDASKZG_BYTES_PER_PROOF 48L
 #undef ethereum_cryptography_LibPeerDASKZG_BYTES_PER_FIELD_ELEMENT
 #define ethereum_cryptography_LibPeerDASKZG_BYTES_PER_FIELD_ELEMENT 32L
-#undef ethereum_cryptography_LibPeerDASKZG_BITS_PER_FIELD_ELEMENT
-#define ethereum_cryptography_LibPeerDASKZG_BITS_PER_FIELD_ELEMENT 255L
-#undef ethereum_cryptography_LibPeerDASKZG_FIELD_ELEMENTS_PER_BLOB
-#define ethereum_cryptography_LibPeerDASKZG_FIELD_ELEMENTS_PER_BLOB 4096L
 #undef ethereum_cryptography_LibPeerDASKZG_BYTES_PER_BLOB
 #define ethereum_cryptography_LibPeerDASKZG_BYTES_PER_BLOB 131072L
-#undef ethereum_cryptography_LibPeerDASKZG_FIELD_ELEMENTS_PER_EXT_BLOB
-#define ethereum_cryptography_LibPeerDASKZG_FIELD_ELEMENTS_PER_EXT_BLOB 8192L
-#undef ethereum_cryptography_LibPeerDASKZG_FIELD_ELEMENTS_PER_CELL
-#define ethereum_cryptography_LibPeerDASKZG_FIELD_ELEMENTS_PER_CELL 64L
-#undef ethereum_cryptography_LibPeerDASKZG_CELLS_PER_EXT_BLOB
-#define ethereum_cryptography_LibPeerDASKZG_CELLS_PER_EXT_BLOB 128L
+#undef ethereum_cryptography_LibPeerDASKZG_NUM_COLUMNS
+#define ethereum_cryptography_LibPeerDASKZG_NUM_COLUMNS 128L
 #undef ethereum_cryptography_LibPeerDASKZG_BYTES_PER_CELL
-#define ethereum_cryptography_LibPeerDASKZG_BYTES_PER_CELL 2048L
+#define ethereum_cryptography_LibPeerDASKZG_BYTES_PER_CELL 262144L
 /*
  * Class:     ethereum_cryptography_LibPeerDASKZG
  * Method:    peerDASContextNew
@@ -46,14 +34,6 @@ JNIEXPORT jlong JNICALL Java_ethereum_cryptography_LibPeerDASKZG_peerDASContextN
  */
 JNIEXPORT void JNICALL Java_ethereum_cryptography_LibPeerDASKZG_peerDASContextDestroy
   (JNIEnv *, jclass, jlong);
-
-/*
- * Class:     ethereum_cryptography_LibPeerDASKZG
- * Method:    computeCells
- * Signature: (J[B)[B
- */
-JNIEXPORT jbyteArray JNICALL Java_ethereum_cryptography_LibPeerDASKZG_computeCells
-  (JNIEnv *, jclass, jlong, jbyteArray);
 
 /*
  * Class:     ethereum_cryptography_LibPeerDASKZG
@@ -82,18 +62,18 @@ JNIEXPORT jboolean JNICALL Java_ethereum_cryptography_LibPeerDASKZG_verifyCellKZ
 /*
  * Class:     ethereum_cryptography_LibPeerDASKZG
  * Method:    verifyCellKZGProofBatch
- * Signature: (J[B[J[J[B[B)Z
+ * Signature: (J[[B[J[J[[B[[B)Z
  */
 JNIEXPORT jboolean JNICALL Java_ethereum_cryptography_LibPeerDASKZG_verifyCellKZGProofBatch
-  (JNIEnv *, jclass, jlong, jbyteArray, jlongArray, jlongArray, jbyteArray, jbyteArray);
+  (JNIEnv *, jclass, jlong, jobjectArray, jlongArray, jlongArray, jobjectArray, jobjectArray);
 
 /*
  * Class:     ethereum_cryptography_LibPeerDASKZG
- * Method:    recoverAllCells
- * Signature: (J[J[B)[B
+ * Method:    recoverCellsAndProof
+ * Signature: (J[J[[B)Lethereum/cryptography/CellsAndProofs;
  */
-JNIEXPORT jbyteArray JNICALL Java_ethereum_cryptography_LibPeerDASKZG_recoverAllCells
-  (JNIEnv *, jclass, jlong, jlongArray, jbyteArray);
+JNIEXPORT jobject JNICALL Java_ethereum_cryptography_LibPeerDASKZG_recoverCellsAndProof
+  (JNIEnv *, jclass, jlong, jlongArray, jobjectArray);
 
 #ifdef __cplusplus
 }
