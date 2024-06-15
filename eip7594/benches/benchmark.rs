9a8b6a3ec819a114358b4be08854f2e2feeddc61
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-06-15 18:17:44 +0100

    chore: bench compute_cells_and_kzg_proofs

--- eip7594/benches/benchmark.rs
+++ eip7594/benches/benchmark.rs
@@ -1,58 +1,27 @@
 use bls12_381::Scalar;
 use criterion::{criterion_group, criterion_main, Criterion};
-use kzg_multi_open::{
-    create_eth_commit_opening_keys, fk20::naive, polynomial::domain::Domain, reverse_bit_order,
-};
+use eip7594::prover::ProverContext;
 
 /// This is here for reference, same as the above `bench_compute_proof_without_fk20`.
-pub fn bench_compute_proof_with_naive_fk20(c: &mut Criterion) {
+pub fn bench_compute_cells_and_kzg_proofs(c: &mut Criterion) {
     const POLYNOMIAL_LEN: usize = 4096;
 
-    let mut polynomial_4096: Vec<_> = (0..POLYNOMIAL_LEN)
+    let prover_context = ProverContext::with_num_threads(100);
+
+    let polynomial_4096: Vec<_> = (0..POLYNOMIAL_LEN)
         .map(|i| -Scalar::from(i as u64))
         .collect();
-    reverse_bit_order(&mut polynomial_4096);
-    let domain = Domain::new(POLYNOMIAL_LEN);
-    let polynomial_4096 = domain.ifft_scalars(polynomial_4096);
 
-    let (ck, _) = create_eth_commit_opening_keys();
-    const NUMBER_OF_POINTS_TO_EVALUATE: usize = 2 * POLYNOMIAL_LEN;
-
-    const NUMBER_OF_POINTS_PER_PROOF: usize = 64;
-    let domain_extended = Domain::new(NUMBER_OF_POINTS_TO_EVALUATE);
-    let mut domain_extended_roots = domain_extended.roots.clone();
-    reverse_bit_order(&mut domain_extended_roots);
-
-    let chunked_bit_reversed_roots: Vec<_> = domain_extended_roots
-        .chunks(NUMBER_OF_POINTS_PER_PROOF)
+    let blob: Vec<_> = polynomial_4096
+        .into_iter()
+        .flat_map(|scalar| scalar.to_bytes_be())
         .collect();
-    let proof_domain = Domain::new(chunked_bit_reversed_roots.len());
+    let blob = &blob.try_into().unwrap();
 
-    c.bench_function(
-        &format!(
-            "computing proofs. POLY_SIZE {}, NUM_INPUT_POINTS {}, NUM_PROOFS {}",
-            POLYNOMIAL_LEN,
-            NUMBER_OF_POINTS_PER_PROOF,
-            chunked_bit_reversed_roots.len()
-        ),
-        |b| {
-            b.iter(|| {
-                naive::fk20_open_multi_point(
-                    &ck,
-                    &proof_domain,
-                    &domain_extended,
-                    &polynomial_4096,
-                    NUMBER_OF_POINTS_PER_PROOF,
-                )
-            })
-        },
-    );
+    c.bench_function("computing cells_and_kzg_proofs", |b| {
+        b.iter(|| prover_context.compute_cells_and_kzg_proofs(blob))
+    });
 }
 
-criterion_group!(
-    benches,
-    // bench_msm,
-    // bench_compute_proof_without_fk20,
-    bench_compute_proof_with_naive_fk20
-);
+criterion_group!(benches, bench_compute_cells_and_kzg_proofs);
 criterion_main!(benches);
