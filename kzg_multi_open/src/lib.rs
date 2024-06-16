16f61ad3039757152143ee42aeee0722fb450374
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-06-16 16:14:42 +0100

    chore: remove serialization test for eth trusted setup and fix linter error with deprecated method

--- kzg_multi_open/src/lib.rs
+++ kzg_multi_open/src/lib.rs
@@ -18,6 +18,7 @@ pub mod eth_trusted_setup;
 /// This is a placeholder method for creating the commit and opening keys for the ethereum
 /// ceremony. This will be replaced with a method that reads the trusted setup file at a higher
 /// level.
+#[allow(deprecated)]
 pub fn create_eth_commit_opening_keys() -> (CommitKey, OpeningKey) {
     let (g1s, g2s) = eth_trusted_setup::deserialize();
     // The setup needs 65 g1 elements for the opening key, in order
@@ -28,21 +29,6 @@ pub fn create_eth_commit_opening_keys() -> (CommitKey, OpeningKey) {
 
     let vk = OpeningKey::new(g1s_65, g2s);
     (ck, vk)
-}
-
-#[cfg(test)]
-mod eth_tests {
-    use super::CommitKey;
-    use crate::{eth_trusted_setup, opening_key::OpeningKey};
-
-    #[test]
-    fn eth_trusted_setup_deserializes() {
-        // Just test that the trusted setup can be loaded/deserialized
-        let (g1s, g2s) = eth_trusted_setup::deserialize();
-        let g1s_65 = g1s[0..65].to_vec();
-        let _ck = CommitKey::new(g1s);
-        let _vk = OpeningKey::new(g1s_65, g2s);
-    }
 }
 
 // Taken and modified from: https://github.com/filecoin-project/ec-gpu/blob/bdde768d0613ae546524c5612e2ad576a646e036/ec-gpu-gen/src/fft_cpu.rs#L10C8-L10C18
