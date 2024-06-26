497011636fff38e76c415e7db3f94703a54725cf
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-06-26 10:24:00 +0100

    chore: add TODO re removing proof check

--- eip7594/src/prover.rs
+++ eip7594/src/prover.rs
@@ -177,6 +177,7 @@ impl ProverContext {
         _proofs: Vec<Bytes48Ref>,
     ) -> Result<([Cell; CELLS_PER_EXT_BLOB], [KZGProof; CELLS_PER_EXT_BLOB]), ProverError> {
         self.thread_pool.install(|| {
+            // TODO: Eventually this will be removed in consensus-specs
             // Check proofs are valid so when test vectors are added
             // this passes, even though we so not use them.
             for proof in _proofs {
