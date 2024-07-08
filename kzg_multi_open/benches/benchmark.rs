fd493b4b43a65568d8dc480cc5dd6f16112d8a22
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-07-08 22:22:14 +0100

    chore: remove unused code

--- kzg_multi_open/benches/benchmark.rs
+++ kzg_multi_open/benches/benchmark.rs
@@ -2,9 +2,8 @@ use bls12_381::lincomb::{g1_lincomb, g1_lincomb_unsafe, g2_lincomb, g2_lincomb_u
 use bls12_381::{ff::Field, group::Group, G1Projective};
 use bls12_381::{G2Projective, Scalar};
 use crate_crypto_kzg_multi_open_fk20::create_eth_commit_opening_keys;
-use crate_crypto_kzg_multi_open_fk20::fk20::{reverse_bit_order, FK20};
+use crate_crypto_kzg_multi_open_fk20::fk20::FK20;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use polynomial::domain::Domain;
 
 pub fn bench_msm(c: &mut Criterion) {
     const NUM_G1_ELEMENTS: usize = 4096;
@@ -45,9 +44,6 @@ pub fn bench_compute_proof(c: &mut Criterion) {
     const NUMBER_OF_POINTS_TO_EVALUATE: usize = 2 * POLYNOMIAL_LEN;
 
     const NUMBER_OF_POINTS_PER_PROOF: usize = 64;
-    let domain_extended = Domain::new(NUMBER_OF_POINTS_TO_EVALUATE);
-    let mut domain_extended_roots = domain_extended.roots.clone();
-    reverse_bit_order(&mut domain_extended_roots);
 
     let fk20 = FK20::new(
         ck,
