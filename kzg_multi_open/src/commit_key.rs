87b816088179244b221af11e1686e6ab742462ea
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-07-08 21:33:54 +0100

    chore: remove CommitKeyLagrange

--- kzg_multi_open/src/commit_key.rs
+++ kzg_multi_open/src/commit_key.rs
@@ -1,6 +1,5 @@
 use bls12_381::lincomb::g1_lincomb;
 use bls12_381::{G1Projective, Scalar};
-use polynomial::domain::Domain;
 
 // The key that is used to commit to polynomials in monomial form
 //
@@ -10,18 +9,6 @@ use polynomial::domain::Domain;
 /// - `G` is some generator of the group
 #[derive(Debug, Clone)]
 pub struct CommitKey {
-    pub g1s: Vec<G1Projective>,
-}
-
-// The key that is used to commit to polynomials in lagrange form
-//
-/// The G1 group elements are of the form `{ \L_i(\tau) * G }`
-/// Where :
-/// - `i` ranges from 0 to `degree`
-/// -  L_i is the i'th lagrange polynomial
-/// - `G` is some generator of the group
-#[derive(Debug)]
-pub struct CommitKeyLagrange {
     pub g1s: Vec<G1Projective>,
 }
 
@@ -35,15 +22,6 @@ impl CommitKey {
         CommitKey { g1s: g1_points }
     }
 
-    /// Convert the `CommitKey` to a `CommitKeyLagrange`
-    ///
-    /// This is done by computing the lagrange basis of the G1 group elements
-    pub fn into_lagrange(self, domain: &Domain) -> CommitKeyLagrange {
-        CommitKeyLagrange {
-            g1s: domain.ifft_g1(self.g1s),
-        }
-    }
-
     /// Commit to `polynomial` in monomial form using the G1 group elements
     pub fn commit_g1(&self, poly_coeff: &[Scalar]) -> G1Projective {
         // Note: We could use g1_lincomb_unsafe here, because we know that none of the points are the
@@ -51,18 +29,5 @@ impl CommitKey {
         // We use g1_lincomb because it is safer and the performance difference is negligible
         g1_lincomb(&self.g1s[0..poly_coeff.len()], poly_coeff)
             .expect("number of g1 points is equal to the number of coefficients in the polynomial")
-    }
-}
-
-impl CommitKeyLagrange {
-    pub fn new(g1s: Vec<G1Projective>) -> CommitKeyLagrange {
-        CommitKeyLagrange { g1s }
-    }
-
-    /// Commit to a polynomial in lagrange form using the G1 group elements
-    pub fn commit_g1(&self, polynomial: &[Scalar]) -> G1Projective {
-        assert!(self.g1s.len() >= polynomial.len());
-        g1_lincomb(&self.g1s[0..polynomial.len()], polynomial)
-            .expect("number of g1 points is equal to the number of evaluations in the polynomial")
     }
 }
