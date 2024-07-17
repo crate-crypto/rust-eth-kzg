4025c0bda6de0ea40c227e60b3da4c66ece43dab
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-07-17 10:15:52 +0100

    chore: add new error type

--- eip7594/src/errors.rs
+++ eip7594/src/errors.rs
@@ -2,19 +2,36 @@ use erasure_codes::errors::RSError;
 
 use crate::CellIndex;
 
+/// Errors that can occur either during proving or verification.
+#[derive(Debug)]
+pub enum Error {
+    Prover(ProverError),
+    VerifierError(VerifierError),
+    Serialization(SerializationError),
+}
+
+impl From<ProverError> for Error {
+    fn from(value: ProverError) -> Self {
+        Error::Prover(value)
+    }
+}
+impl From<VerifierError> for Error {
+    fn from(value: VerifierError) -> Self {
+        Error::VerifierError(value)
+    }
+}
+impl From<SerializationError> for Error {
+    fn from(value: SerializationError) -> Self {
+        Error::Serialization(value)
+    }
+}
+
 /// Errors that can occur while calling a method in the Prover API
 #[derive(Debug)]
 pub enum ProverError {
     // TODO: This will be getting removed, waiting for consensus-specs PR
     NumProofsDoesNotEqualNumCells,
-    Serialization(SerializationError),
     RecoveryFailure(VerifierError),
-}
-
-impl From<SerializationError> for ProverError {
-    fn from(value: SerializationError) -> Self {
-        ProverError::Serialization(value)
-    }
 }
 
 impl From<VerifierError> for ProverError {
@@ -26,7 +43,6 @@ impl From<VerifierError> for ProverError {
 /// Errors that can occur while calling a method in the Verifier API
 #[derive(Debug)]
 pub enum VerifierError {
-    Serialization(SerializationError),
     NumCellIndicesNotEqualToNumCells {
         num_cell_indices: usize,
         num_cells: usize,
@@ -70,12 +86,6 @@ pub enum VerifierError {
 impl From<RSError> for VerifierError {
     fn from(value: RSError) -> Self {
         VerifierError::ReedSolomon(value)
-    }
-}
-
-impl From<SerializationError> for VerifierError {
-    fn from(value: SerializationError) -> Self {
-        VerifierError::Serialization(value)
     }
 }
 
