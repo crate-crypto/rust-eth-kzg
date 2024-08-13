2940b3cf5ca1972e32e25834f54b78186bf8d04a
kevaundray <kevtheappdev@gmail.com>
2024-08-13 18:16:02 +0100

    add benchmarks for bls12-381 (#152)

--- cryptography/bls12_381/benches/benchmark.rs
+++ cryptography/bls12_381/benches/benchmark.rs
@@ -1,6 +1,10 @@
-use crate_crypto_internal_eth_kzg_bls12_381::ff::Field;
-use crate_crypto_internal_eth_kzg_bls12_381::{batch_inversion, Scalar};
+use blstrs::G1Affine;
+use crate_crypto_internal_eth_kzg_bls12_381::{
+    batch_inversion, ff::Field, fixed_base_msm::FixedBaseMSM, group::Group, lincomb::g1_lincomb,
+    G1Projective, Scalar,
+};
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use rand::thread_rng;
 
 pub fn batch_inversion(c: &mut Criterion) {
     const NUM_ELEMENTS: usize = 8192;
@@ -16,6 +20,33 @@ pub fn batch_inversion(c: &mut Criterion) {
         },
     );
 }
+pub fn fixed_base_msm(c: &mut Criterion) {
+    let length = 64;
+    let generators: Vec<_> = (0..length)
+        .map(|_| G1Projective::random(&mut rand::thread_rng()).into())
+        .collect();
+    let fbm = FixedBaseMSM::new(generators, 8);
+    let scalars: Vec<_> = (0..length)
+        .map(|_| Scalar::random(&mut thread_rng()))
+        .collect();
 
-criterion_group!(benches, batch_inversion);
+    c.bench_function("bls12_381 fixed_base_msm length=64 width=8", |b| {
+        b.iter(|| fbm.msm(scalars.clone()))
+    });
+}
+pub fn g1_lincomb_safe(c: &mut Criterion) {
+    let length = 64;
+    let generators: Vec<G1Affine> = (0..length)
+        .map(|_| G1Projective::random(&mut rand::thread_rng()).into())
+        .collect();
+    let scalars: Vec<_> = (0..length)
+        .map(|_| Scalar::random(&mut thread_rng()))
+        .collect();
+
+    c.bench_function("g1_lincomb length=64", |b| {
+        b.iter(|| g1_lincomb(&generators, &scalars))
+    });
+}
+
+criterion_group!(benches, batch_inversion, fixed_base_msm, g1_lincomb_safe);
 criterion_main!(benches);
