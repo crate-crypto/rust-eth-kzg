992c75ebea5bac572a92e62c2700f021ec44c7a7
Thomas Coratger <60488569+tcoratger@users.noreply.github.com>
2025-05-12 21:47:37 +0200

    chore: small doc fix (#370)

--- crates/eip7594/src/constants.rs
+++ crates/eip7594/src/constants.rs
@@ -3,7 +3,7 @@
 
 /// The number of bytes needed to represent a field element.
 ///
-/// Note: This is originally specified in the 4844 specs.
+/// Note: This is originally specified in the eip-4844 specs.
 ///
 /// See: https://github.com/ethereum/EIPs/blob/master/EIPS/eip-4844.md
 pub const BYTES_PER_FIELD_ELEMENT: usize = 32;
